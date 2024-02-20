ea7fd5d617d912571eeb0bbc531684a0fb71af1e
tigerplush <64077786+tigerplush@users.noreply.github.com>
2024-02-20 09:00:33 +0100

    Added first functioning version of dice throw endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+use dotenv::dotenv;
+use rocket::serde::{json::Json, Deserialize};
+use rocket::{post, routes};
+
+#[tokio::main]
+async fn main() -> Result<(), rocket::Error> {
+    dotenv().ok();
     println!("Hello, world!");
+    rocket::build()
+        .mount("/", routes![throw_dice])
+        .launch()
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct DiceThrow<'r> {
+    throw: &'r str,
+}
+
+#[post("/dice", data = "<dice_throw>")]
+fn throw_dice(dice_throw: Json<DiceThrow<'_>>) {
+    println!("received dice throw: {}", dice_throw.throw);
 }
